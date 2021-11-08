62ff81726e38d0cc2179cb7b56a7a1a68a749b23
Liz <stokesec@hotmail.com>
2021-11-08 15:20:45 +1300

    New Player Script No Ani

---
+++ Assets/Liz/Scripts/PlayerMovev2noAni.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovev2noAni : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpForce = 300f;
+
+    private float moveHori;
+    private float moveVert;
+    private Vector2 currVelo;
+
+    private bool isJumping = false;
+    private bool hasJumped = false;
+
+    public Rigidbody2D PlayerRb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHori = Input.GetAxis("Horizontal");
+        moveVert = Input.GetAxis("Vertical");
+        currVelo = PlayerRb.velocity;
+
+        isJumping = false;
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            isJumping = true;
+            hasJumped = false;
+            
+        }
+
+
+        if (moveHori != 0)
+        {
+            PlayerRb.velocity = new Vector2(moveHori * speed, currVelo.y);
+        }
+
+        if (isJumping && !hasJumped)
+        {
+            PlayerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+            
+        }
+       
+    }
+   
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Platform")
+        {
+            isJumping = false;
+            
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Platform")
+        {
+            isJumping = true;
+
+        }
+    }
+
+}
